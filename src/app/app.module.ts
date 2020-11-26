cc7d99139339053a8957e2163de670ce482de22d
LeBoku <daniel.zuberbuehler@outlook.com>
2020-11-26 22:06:53 +0100

    added pwa functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppComponent } from './app.component';
 import { StatsPaneComponent } from './stats-pane/stats-pane.component';
 import { RulesComponent } from './rules/rules.component';
 import { TruncatedNumberPipe } from './truncated-number.pipe';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -14,7 +16,8 @@ import { TruncatedNumberPipe } from './truncated-number.pipe';
 		TruncatedNumberPipe
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
 	],
 	providers: [],
 	bootstrap: [AppComponent]
